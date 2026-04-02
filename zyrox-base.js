86176bc9a6ad84ec927175b7c376ab41e8170781
Bob <bobaltco@proton.me>
2026-04-03 00:22:11 +0200

    Update zyrox-base.js

--- zyrox-base.js
+++ zyrox-base.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Zyrox client (gimkit)
 // @namespace    https://github.com/zyrox
-// @version      0.9.1
+// @version      0.9.2
 // @description  Modern UI/menu shell for Zyrox client
 // @author       Zyrox
 // @match        https://www.gimkit.com/join*
@@ -45,7 +45,7 @@
 
   function readUserscriptVersion() {
     // Update this variable whenever you bump @version above.
-    const CLIENT_VERSION = "0.9.1";
+    const CLIENT_VERSION = "0.9.2";
     return CLIENT_VERSION;
   }
 
