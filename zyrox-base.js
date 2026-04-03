bf96697ea0b5ba61729383ab1b0bb61aa3c3851c
My-codex-ran-out <codex-ran-out-1@proton.me>
2026-04-03 21:42:32 +0200

    Update zyrox-base.js

--- zyrox-base.js
+++ zyrox-base.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Zyrox client (gimkit)
 // @namespace    https://github.com/zyrox
-// @version      1.1.9
+// @version      1.2.0
 // @description  Modern UI/menu shell for Zyrox client
 // @author       Zyrox
 // @match        https://www.gimkit.com/join*
@@ -376,7 +376,7 @@
 
   function readUserscriptVersion() {
     // Update this variable whenever you bump @version above.
-    const CLIENT_VERSION = "1.1.9";
+    const CLIENT_VERSION = "1.2.0";
     return CLIENT_VERSION;
   }
 
