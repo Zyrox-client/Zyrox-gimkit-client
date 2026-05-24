8edf719768c7f6a3827e2ce1ecc48f85fec32678
Zyrox client <zyrox-client@proton.me>
2026-05-24 22:27:24 +0200

    Update zyrox-base.js

--- zyrox-base.js
+++ zyrox-base.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Zyrox client (gimkit)
 // @namespace    https://github.com/Zyrox-client
-// @version      2.7.4
+// @version      2.7.5
 // @description  A modern userscript hacked client for gimkit
 // @author       Zyrox client
 // @match        https://www.gimkit.com/join*
@@ -599,7 +599,7 @@
 
   function readUserscriptVersion() {
     
-    const CLIENT_VERSION = "2.7.4";
+    const CLIENT_VERSION = "2.7.5";
     return CLIENT_VERSION;
   }
 
